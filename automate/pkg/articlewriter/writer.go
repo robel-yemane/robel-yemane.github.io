c8dfb23f4e00cfe458dd3c57377c8ca6ff7e46d4
Robel Yemane <ryhgb03@gmail.com>
2022-08-12 14:21:03 +0100

    chore: app layout changes (#27)
    * chore: code layout restructure

--- automate/pkg/articlewriter/writer.go
+++ automate/pkg/articlewriter/writer.go
@@ -13,19 +13,28 @@ func Write(htmlSkeleton string, data []string, w io.Writer) error {
 	utime := time.Now()
 	utimedate := utime.Format("2006-01-02")
 	ftimedate := utime.Format("2006.01.02")
+	email := "ryhgb03@gmail.com"
+	twitter := "https://twitter.com/robelyemane_"
+	linkedin := "https://www.linkedin.com/in/ryemane/"
 
 	htmlLayout := struct {
-		Title  string
-		Header string
-		Udate  string
-		Fdate  string
-		Body   []string
+		Title    string
+		Header   string
+		Udate    string
+		Fdate    string
+		Body     []string
+		Email    string
+		Twitter  string
+		Linkedin string
 	}{
-		Title:  "Robel Yemane",
-		Header: "Lorem Ipsum",
-		Udate:  utimedate,
-		Fdate:  ftimedate,
-		Body:   data,
+		Title:    "Robel Yemane",
+		Header:   "Lorem Ipsum",
+		Udate:    utimedate,
+		Fdate:    ftimedate,
+		Body:     data,
+		Email:    email,
+		Twitter:  twitter,
+		Linkedin: linkedin,
 	}
 	t, err := template.New("blog").Parse(htmlSkeleton)
 	if err != nil {
